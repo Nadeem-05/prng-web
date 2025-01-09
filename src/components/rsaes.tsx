436792f724720f6a39bc7787c09a9267b6ba5327
Srivatsav Auswin <1012srivatsavas@gmail.com>
2025-01-10 00:29:54 +0530

    Added Switch 🎛️

---
+++ src/components/rsaes.tsx
@@ -0,0 +1,7 @@
+export default function RSAES() {
+  return (
+    <main className="w-full flex flex-col justify-center items-center pt-5">
+      <h1 className="text-4xl font-bold">RSAES</h1>
+    </main>
+  );
+}
